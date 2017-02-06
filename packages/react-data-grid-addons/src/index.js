db99ac60c4912fef53d67941f183baa22ec48e3b
Prateek Bhatt <prattbhatt@gmail.com>
2017-02-07 00:25:26 +0530

    export only Data from react-grid-addons to reduce dist size

--- packages/react-data-grid-addons/src/index.js
+++ packages/react-data-grid-addons/src/index.js
@@ -1,14 +1,20 @@
-const Editors = require('./editors');
-const Formatters = require('./formatters');
-const Toolbar = require('./toolbars/Toolbar');
-const ToolsPanel = require('./toolbars');
-const Data = require('./data');
-const Menu = require('./menu');
-const Draggable = require('./draggable');
-const Filters = require('./cells/headerCells/filters');
-const { RowComparer: rowComparer } = require('react-data-grid');
-const performance = require('./performance');
-const Utils = { rowComparer, performance };
+// const Editors = require('./editors');
+// const Formatters = require('./formatters');
+// const Toolbar = require('./toolbars/Toolbar');
+// const ToolsPanel = require('./toolbars');
+// const Data = require('./data');
+// const Menu = require('./menu');
+// const Draggable = require('./draggable');
+// const Filters = require('./cells/headerCells/filters');
+// const { RowComparer: rowComparer } = require('react-data-grid');
+// const performance = require('./performance');
+// const Utils = { rowComparer, performance };
 
-window.ReactDataGridPlugins = {Editors, Formatters, Toolbar, Menu, Data, ToolsPanel, Draggable, Filters, Utils};
-module.exports = {Editors, Formatters, Toolbar, Menu, Data, ToolsPanel, Draggable, Filters, Utils};
+// window.ReactDataGridPlugins = {Editors, Formatters, Toolbar, Menu, Data, ToolsPanel, Draggable, Filters, Utils};
+// module.exports = {Editors, Formatters, Toolbar, Menu, Data, ToolsPanel, Draggable, Filters, Utils};
+
+
+const Data = require('./data');
+
+window.ReactDataGridPlugins = {Data};
+module.exports = {Data};
